cdc39faf167f587eed0dae924f9eb294cab7e50d
ChoiHyoungKyu <109322428+Choihyoungkyu@users.noreply.github.com>
2023-10-30 15:39:36 +0900

    231030

---
+++ mifirsewebapp/src/test/java/com/in28minutes/springboot/mifirsewebapp/MifirsewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.mifirsewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MifirsewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
